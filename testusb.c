0a41ed79f00a92eb03c25e2fdbcc2ed5800ee5df
aka_teraxl <muga.alexandr@yandex.ru>
2022-08-08 01:58:44 +0300

    update

--- testusb.c
+++ testusb.c
@@ -6,15 +6,14 @@
 #include <string.h>
 #include <assert.h>
 
-#define USB_LOCK_VENDOR 0x046d
-#define USB_LOCK_PRODUCT 0x0a45
+#define USB_LOGITEC_VID 0x046d
+#define USB_LOGITEC_PID 0x0a45
+
+#define USB_TOSHIBA_VID 0x0930
+#define USB_TOSHIBA_PID 0x6545
+
 #define DEFAULT_INTERFACE 0x00
 #define DEFAULT_CONFIGURATION 0x01
-
-#define TRUE 1
-#define FALSE 0
-
-FILE *debugout, *verbout;
 
 struct libusb_device_handle *ch341configure(uint16_t vid, uint16_t pid) {
 	
@@ -38,13 +37,10 @@ struct libusb_device_handle *ch341configure(uint16_t vid, uint16_t pid) {
 	libusb_set_option(NULL, LIBUSB_OPTION_LOG_LEVEL, 3);
 #endif
 
-	fprintf(verbout, "Searching USB buses for [%04x:%04x]\n",
-			USB_LOCK_VENDOR, USB_LOCK_PRODUCT);
+	fprintf(stdout, "Searching USB buses for [%04x:%04x]\n", vid, pid);
 
-	if (!(devHandle = libusb_open_device_with_vid_pid(NULL,
-					USB_LOCK_VENDOR, USB_LOCK_PRODUCT))) {
-		fprintf(stderr, "Couldn't open device [%04x:%04x]\n",
-				USB_LOCK_VENDOR, USB_LOCK_PRODUCT);
+	if (!(devHandle = libusb_open_device_with_vid_pid(NULL, vid, pid))) {
+		fprintf(stderr, "Couldn't open device [%04x:%04x]\n", vid, pid);
 		return NULL;
 	}
 
@@ -53,11 +49,9 @@ struct libusb_device_handle *ch341configure(uint16_t vid, uint16_t pid) {
 		return NULL;
 	}
 
-	fprintf(verbout, "Found [%04x:%04x] as device [%d] on USB bus [%d]\n",
-			USB_LOCK_VENDOR, USB_LOCK_PRODUCT,
+	fprintf(stdout, "Found [%04x:%04x] as device [%d] on USB bus [%d]\n", vid, pid,
 			libusb_get_device_address(dev), libusb_get_bus_number(dev));
-	fprintf(verbout, "Open device [%04x:%04x]\n", 
-			USB_LOCK_VENDOR, USB_LOCK_PRODUCT);
+	fprintf(stdout, "Open device [%04x:%04x]\n", vid, pid);
 
 	if (libusb_kernel_driver_active(devHandle, DEFAULT_INTERFACE)) {
 		ret = libusb_detach_kernel_driver(devHandle, DEFAULT_INTERFACE);
@@ -66,7 +60,7 @@ struct libusb_device_handle *ch341configure(uint16_t vid, uint16_t pid) {
 					strerror(-ret));
 			return NULL;
 		} else {
-			fprintf(verbout, "Detached kernel driver\n");
+			fprintf(stdout, "Detached kernel driver\n");
 		}
 	}
 
@@ -95,7 +89,7 @@ struct libusb_device_handle *ch341configure(uint16_t vid, uint16_t pid) {
 		return NULL;
 	}
 
-	fprintf(verbout, "Claim device interface [%d]\n", DEFAULT_INTERFACE);
+	fprintf(stdout, "Claim device interface [%d]\n", DEFAULT_INTERFACE);
 
 	ret = libusb_get_descriptor(devHandle, LIBUSB_DT_DEVICE,
 			0x00, ch341DescriptorBuffer, 0x12);
@@ -106,14 +100,14 @@ struct libusb_device_handle *ch341configure(uint16_t vid, uint16_t pid) {
 		return NULL;
 	}
 
-	fprintf(verbout, "Device reported its revision [%d.%02d]\n",
+	fprintf(stdout, "Device reported its revision [%d.%02d]\n",
 			ch341DescriptorBuffer[12], ch341DescriptorBuffer[13]);
 	
 	for (i=0; i < 0x12; i++) {
-		fprintf(debugout, "%02x ", ch341DescriptorBuffer[i]);	
+		fprintf(stdout, "%02x ", ch341DescriptorBuffer[i]);	
 	}
 	
-	fprintf(debugout, "\n");
+	fprintf(stdout, "\n");
 
 	return devHandle;
 }
@@ -121,15 +115,14 @@ struct libusb_device_handle *ch341configure(uint16_t vid, uint16_t pid) {
 int main(int argc, char **argv) {
 	
 	struct libusb_device_handle *devHandle = NULL;
-	uint8_t debug = FALSE, verbose = FALSE;
-	debugout = (debug == TRUE) ? stdout : fopen("/dev/null", "w");
-	verbout = (verbose == TRUE) ? stdout : fopen("/dev/null", "w");
-	fprintf(debugout, "Debug Enabled\n");
-
+	struct libusb_device *dev = NULL;
+	devHandle = ch341configure(USB_LOGITEC_VID, USB_LOGITEC_PID);
+	dev = libusb_get_device(devHandle);
+	uint8_t address = libusb_get_device_address(dev);
+	int device_speed = libusb_get_device_speed(dev);
+	fprintf(stdout, "Address: %d\n", address);
+	fprintf(stdout, "Speed: %d\n", device_speed);
 	
-	devHandle = ch341configure(USB_LOCK_VENDOR, USB_LOCK_PRODUCT);
-	
-
 	libusb_release_interface(devHandle, DEFAULT_INTERFACE);
 	libusb_close(devHandle);
 	libusb_exit(NULL);
